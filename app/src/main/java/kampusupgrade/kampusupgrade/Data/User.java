22773f50c37b2a0a1fdd26f3aec7edb0d6b838ed
Mathijs van Lindenberg <mathijs.van.lindenberg@hva.nl>
2017-03-19 00:01:09 +0200

    helper classes and start of RestClient

---
+++ app/src/main/java/kampusupgrade/kampusupgrade/Data/User.java
@@ -0,0 +1,8 @@
+package kampusupgrade.kampusupgrade.Data;
+
+/**
+ * Created by Mathijs on 18-3-2017.
+ */
+
+public class User {
+}
---
+++ app/src/main/java/kampusupgrade/kampusupgrade/RestClient/RestClient.java
@@ -0,0 +1,12 @@
+package kampusupgrade.kampusupgrade.RestServer;
+
+/**
+ * Created by Mathijs on 18-3-2017.
+ */
+
+public class RestServer {
+
+
+
+
+}
